f1aa3a3bab105e8a5311ac6f91b130ffb62c2b07
NakanoRyo <NakanoRyo@192.168.10.5>
2019-05-26 23:31:21 +0900

    ソース内の不要コメント削除

--- src/dao/ConnectionFactory.java
+++ src/dao/ConnectionFactory.java
@@ -10,7 +10,7 @@ public final class ConnectionFactory {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			return DriverManager.getConnection("jdbc:mysql://localhost:3306/javasystem?"
-					+ "autoReconnect=true&useSSL=false", // この辺は自環境で再現する時怪しい気がする・・・
+					+ "autoReconnect=true&useSSL=false",
 	        		"root","mySql108yen");
 
 		} catch (ClassNotFoundException e) {
